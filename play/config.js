53a823613515171b09445e71436c182405b19b08
xueyuanhuang <151151360+xueyuanhuang@users.noreply.github.com>
2026-08-05 15:32:59 +0800

    chore: remove online play mode

--- play/config.js
+++
@@ -1,4 +0,0 @@
-// API base for poker-play Worker.
-window.PLAY_CONFIG = {
-  apiBase: localStorage.getItem("PLAY_API_BASE") || "https://poker-play.xue-yuanhuang.workers.dev",
-};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PWA_CACHE_VERSION = '2026.08.04-cash-leaderboard-details.1';
+const PWA_CACHE_VERSION = '2026.08.05-remove-play.1';
 const STATIC_CACHE = `texasholdem-static-${PWA_CACHE_VERSION}`;
 const RUNTIME_CACHE = `texasholdem-runtime-${PWA_CACHE_VERSION}`;
 
