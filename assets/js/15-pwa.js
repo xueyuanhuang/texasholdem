00b1af6d7c27d14488a7acba9c47096bc8988383
yuanhuang <yuanhuang@yuanhuangs-Mac-mini.local>
2026-09-13 11:33:01 +0800

    Apply club control styling inside player details

--- assets/js/15-pwa.js
+++ assets/js/15-pwa.js
@@ -1,5 +1,5 @@
 // ====== PWA Install + Update ======
-const PWA_APP_VERSION = '2026.09.13-player-details.1';
+const PWA_APP_VERSION = '2026.09.13-player-details.2';
 
 let pwaRegistration = null;
 let pendingPwaWorker = null;
